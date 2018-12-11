1f463093bc64561c524a059682b7bb8e77f90692
joeaespi <joel_es_@hotmail.com>
2018-12-11 12:38:12 -0500

    creacion de tds node y ab

--- src/espol/edu/ec/tda/ArbolBinario/AB.java
+++ src/espol/edu/ec/tda/ArbolBinario/AB.java
@@ -9,6 +9,27 @@ package espol.edu.ec.tda.ArbolBinario;
  *
  * @author joel
  */
-public class AB {
+public class AB<E> {
+    private Node<E> root;
+    public AB(){
+        root= null;
+    }
     
+    public boolean isEmpty(){
+        return root==null;
+    }
+    
+    private Node<E> searchNode(E data){
+        return searchNode(data,root);
+    }
+    
+    private Node<E> searchNode(E data, Node<E> node){
+        if(node==null){
+            return null;
+        }else if (node.getData().equals(data)){
+            return node;
+        }else {
+            
+        }
+    }
 }
--- src/espol/edu/ec/tda/ArbolBinario/Node.java
+++ src/espol/edu/ec/tda/ArbolBinario/Node.java
@@ -9,6 +9,38 @@ package espol.edu.ec.tda.ArbolBinario;
  *
  * @author joel
  */
-public class Node {
+public class Node<E> {
+    private E data;
+    private Node<E> left;
+    private Node<E> right;
+    
+    public Node(E data){
+        this.data=data;
+        left=right=null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
     
 }
