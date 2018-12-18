2cb598ce4fdeae158b11aa79a98de6bde56b6f96
joeaespi <joel_es_@hotmail.com>
2018-12-18 13:40:51 -0500

    taller

--- src/espol/edu/ec/tda/ArbolBusqueda/ABB.java
+++ src/espol/edu/ec/tda/ArbolBusqueda/ABB.java
@@ -44,4 +44,92 @@ public class ABB<E> {
         return p;
     }
     
+    public boolean contains(E data){
+        if(data == null || this.isEmpty()){
+            return false;
+        }else{
+            return constains(data,root);
+        }
+    }
+    
+    private boolean constains(E data, Node<E> p){
+        if(p == null){
+            return false;
+        }else if(data == p.getData()){
+            return true;
+        }else if(f.compare(data, p.getData())>0){
+            return constains(data,p.getRight());
+        }else if(f.compare(data, p.getData())< 0){
+            return constains(data,p.getLeft());
+        }
+        return false;
+    }
+    
+    public E max(){
+        if(this.isEmpty()){
+            return null;
+        }else {
+            return max(root);
+        }
+    }
+    
+    private E max(Node<E> p ){
+        if(p.getRight()==null){
+            return p.getData();
+        }else{
+            return max(p.getRight());
+        }
+    }
+    
+    public E min(){
+        if(this.isEmpty()){
+            return null;
+        }else {
+            return min(root);
+        }
+    }
+    
+    private E min(Node<E> p ){
+        if(p.getLeft()==null){
+            return p.getData();
+        }else{
+            return max(p.getLeft());
+        }
+    }
+    
+    public void preOrder(){
+        preOrder(root);
+    }
+    
+    private void preOrder(Node<E> p){
+        if(p!=null){
+            System.out.print(p.getData()+" ");
+            preOrder(p.getLeft());
+            preOrder(p.getRight());
+        }
+    }
+    
+    public void posOrder(){
+        posOrder(root);
+    }
+    
+    private void posOrder(Node<E> p){
+        if(p!=null){
+            posOrder(p.getLeft());
+            posOrder(p.getRight());
+            System.out.print(p.getData()+" " );
+        }
+    }
+    
+    public void inOrder(){
+        inOrder(root);
+    }
+    
+    private void inOrder(Node<E> p){
+        if(p!=null){
+            inOrder(p.getLeft());
+            System.out.print(p.getData()+" ");
+            inOrder(p.getRight());
+        }
+    }
 }
---
+++ src/espol/edu/ec/tda/ArbolBusqueda/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package espol.edu.ec.tda.ArbolBusqueda;
+
+/**
+ *
+ * @author joel
+ */
+public class Main {
+    public static void main(String[] args) {
+        ABB<Integer> ar = new ABB<>(Integer::compareTo);
+        ar.add(8);
+        ar.add(19);
+        ar.add(9);
+        ar.add(7);
+        ar.add(5);
+        ar.add(3);
+        ar.add(1);
+        ar.inOrder();
+        System.out.print("\n");
+        System.out.println(ar.max());
+        System.out.println(ar.min());
+        System.out.println(ar.contains(20));
+        System.out.println(ar.contains(19));
+    }
+}
